7b828607bd86a4d5f054e4a694f58733d80ccc83
Kartik Shenoy <kartik.shenoyy@gmail.com>
2022-09-28 17:35:29 -0500

    fix: updated setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="DAIDE",
+    name="stance_vector",
     version="0.0.1",
     author="Kartik Shenoy",
     author_email="kartik.shenoyy@gmail.com",
