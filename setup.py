8b2d974a68c752d4a31be68ca0c579d7cc1d94fd
Alex Hedges <ahedges@isi.edu>
2023-03-09 19:49:00 -0500

    Switch to declarative configuration file
    `setup.cfg` is simpler than `setup.py` and does not require executing
    any code to process. `setup.py` should only be used for data that cannot
    be expressed statically.

--- setup.py
+++ setup.py
@@ -1,23 +1,5 @@
-import setuptools
+"""A setuptools-based setup module for stance_vector."""
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+from setuptools import setup
 
-setuptools.setup(
-    name="stance_vector",
-    version="0.0.1",
-    author="Kartik Shenoy",
-    author_email="kartik.shenoyy@gmail.com",
-    description="Stance Vector Computation for Diplomacy Board Game",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="NO",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
-)
+setup()
