4a65ac5282882d7699f7e03839b89f22ebd48902
Alex Hedges <ahedges@isi.edu>
2023-03-15 19:50:54 -0400

    Improve code in `score_based_stance.py`
    The most significant change was to set default values for the `scores`
    and `stance` fields so they can be safely read at any point. I also
    greatly simplified the logic in `extract_scores()` and replaced the
    usage of `numpy.sign()` in `get_stance()` with what is hopefully
    easier-to-understand logic.

--- src/stance_vector/score_based_stance.py
+++ src/stance_vector/score_based_stance.py
@@ -1,54 +1,48 @@
-from typing import Dict, Optional
+from itertools import product
+from typing import Dict
 
 from diplomacy import Game
-import numpy as np
 
 from .stance_extraction import StanceExtraction
 
 
 class ScoreBasedStance(StanceExtraction):
-    """
-    A turn-level score-based subjective stance vector baseline
+    """A turn-level score-based subjective stance vector baseline.
+
     "Whoever stronger than me must be evil, whoever weaker than me can be my ally."
-    Stance on nation k =
-        sign(my score - k's score)
+
+    Stance on nation k = sign(my score - k's score)
     """
 
-    scores: Optional[Dict[str, int]]
-    stance: Optional[Dict[str, Dict[str, float]]]  # type: ignore[assignment]
+    scores: Dict[str, int]
 
     def __init__(self, my_identity: str, game: Game) -> None:
         super().__init__(my_identity, game)
-        self.scores = None
-        self.stance = None
+        self.scores = {n: 0 for n in self.nations}
+        self.stance = {n: {k: 0 for k in self.nations} for n in self.nations}
 
     def extract_scores(self) -> Dict[str, int]:
-        """
-        Extract scores at the end of each round.
-            game_rec: the turn-level JSON log of a game,
+        """Extract scores at the end of each round.
+
+        A nation's score is the number of centers it controls.
+
         Returns a dict of scores for all nations
         """
-        scores = {n: 0 for n in self.nations}
-        for n in self.nations:
-            scores[n] = len(self.game.powers[n].centers) if self.game.powers[n].centers else 0
-        return scores
+        return {n: len(self.game.get_centers(n)) for n in self.nations}
 
     def get_stance(self) -> Dict[str, Dict[str, float]]:  # type: ignore[override]
-        """
-        Extract turn-level subjective stance of nation n on nation k.
-            game_rec: the turn-level JSON log of a game,
-            messages is not used
+        """Extract turn-level subjective stance of nation n on nation k.
+
         Returns a bi-level dictionary of stance score stance[n][k]
         """
-        # extract territory info
         self.scores = self.extract_scores()
 
-        self.stance = {
-            n: {
-                k: np.sign(self.scores[n] - self.scores[k]) if self.scores[n] > 0 else 0
-                for k in self.nations
-            }
-            for n in self.nations
-        }
+        for n, k in product(self.nations, repeat=2):
+            if self.scores[n] > 0 and self.scores[n] > self.scores[k]:
+                self.stance[n][k] = 1
+            elif self.scores[n] > 0 and self.scores[n] < self.scores[k]:
+                self.stance[n][k] = -1
+            else:
+                self.stance[n][k] = 0
 
         return self.stance
